6a5f638c613980c3cc1664b7b503dca8dfe243cf
lva22u235 <lva22u235@student.bmstu.ru>
2024-03-12 12:46:50 +0300

    Working version with printing biss

--- lab_01_38/find_triangle.py
+++ lab_01_38/find_triangle.py
@@ -1,11 +1,43 @@
-from sympy import Point, Triangle, Line
+from sympy import Triangle, Line
 import math
 def area_triangle(a, b, c):
     s = (a + b + c) / 2
     area = (s * (s - a) * (s - b) * (s - c))**0.5
     return area
 
+def find_biss(side1, side2, side3, p):
+    l_a = (2 * (side2 * side3 * p * (p - side1)) ** 0.5) / (side2 + side3)
+    l_b = (2 * (side1 * side3 * p * (p - side2)) ** 0.5) / (side1 + side3)
+    l_c = (2 * (side1 * side2 * p * (p - side3)) ** 0.5) / (side1 + side2)
+    return l_a, l_b, l_c
+
+def find_part_of_biss(l_a, l_b, l_c, side1, side2, side3):
+    l_a_second = l_a / ((side2 + side3) / side1 + 1)
+    l_a_first = l_a - l_a_second
+    l_b_second = l_b / ((side1 + side3) / side2 + 1)
+    l_b_first = l_b - l_b_second
+    l_c_second = l_c / ((side1 + side2) / side3 + 1)
+    l_c_first = l_c - l_c_second
+    return l_a_first, l_a_second, l_b_first, l_b_second, l_c_first, l_c_second
+
+def find_part_of_sides(side1, side2, side3):
+    side1_second = side1 / (side3 / side2 + 1)
+    side1_first = side1 - side1_second
+    side2_second = side2 / (side1 / side3 + 1)
+    side2_first = side2 - side2_second
+    side3_second = side3 / (side1 / side2 + 1)
+    side3_first = side3 - side3_second
+    return side1_first, side1_second, side2_first, side2_second, side3_first, side3_second
+
+
+def check_triangle(side1, side2, side3):
+    if side1 + side2 > side3 and side1 + side3 > side2 and side2 + side3 > side1:
+        return False
+    return True
+
+    
 def find_min_dif_square(points):
+
 
 
     # Вычисляем длины сторон треугольника по его вершинам
@@ -13,28 +45,20 @@ def find_min_dif_square(points):
     side2 = math.hypot(points[1][0] - points[2][0], points[1][1] - points[2][1])
     side3 = math.hypot(points[0][0] - points[2][0], points[0][1] - points[2][1])
 
+    rc = check_triangle(side1, side2, side3)
+    if rc:
+        return math.inf
+    
     p = (side1 + side2 + side3) / 2
     # Вычисляем длины биссектрис треугольника
-    try:
-        l_a = (2 * (side2 * side3 * p * (p - side1)) ** 0.5) / (side2 + side3)
-        l_b = (2 * (side1 * side3 * p * (p - side2)) ** 0.5) / (side1 + side3)
-        l_c = (2 * (side1 * side2 * p * (p - side3)) ** 0.5) / (side1 + side2)
-        # Вычисляем длину первой и второй части каждой биссектрисы относительно точки пересечения
-        l_a_second = l_a / ((side2 + side3) / side1 + 1)
-        l_a_first = l_a - l_a_second
-        l_b_second = l_b / ((side1 + side3) / side2 + 1)
-        l_b_first = l_b - l_b_second
-        l_c_second = l_c / ((side1 + side2) / side3 + 1)
-        l_c_first = l_c - l_c_second
-        # Вычисляем длину первой и второй части каждой стороны относительно точки, куда падает биссектриса
-        side1_second = side1 / (side3 / side2 + 1)
-        side1_first = side1 - side1_second
-        side2_second = side2 / (side1 / side3 + 1)
-        side2_first = side2 - side2_second
-        side3_second = side3 / (side1 / side2 + 1)
-        side3_first = side3 - side3_second
-    except:
-        return math.inf
+    l_a, l_b, l_c = find_biss(side1, side2, side3, p)
+    
+    # Вычисляем длину первой и второй части каждой биссектрисы относительно точки пересечения
+    l_a_first, l_a_second, l_b_first, l_b_second, l_c_first, l_c_second = find_part_of_biss(l_a, l_b, l_c, side1, side2, side3)
+
+    # Вычисляем длину первой и второй части каждой стороны относительно точки, куда падает биссектриса
+    side1_first, side1_second, side2_first, side2_second, side3_first, side3_second = find_part_of_sides(side1, side2, side3)
+
     # Считаем каждую площадь
     s1 = area_triangle(l_c_second, side3_first, l_b_first)
     s2 = area_triangle(l_c_second, side3_second, l_a_first)
@@ -42,9 +66,7 @@ def find_min_dif_square(points):
     s4 = area_triangle(l_b_second, side2_first, l_c_first)
     s5 = area_triangle(l_a_second, side1_second, l_c_first)
     s6 = area_triangle(l_a_second, side1_first, l_b_first)
-    s = area_triangle(side1, side2, side3)
-    if (type(s1) == complex or type(s2) == complex or type(s3) == complex or type(s4) == complex or type(s5) == complex or type(s6) == complex):
-        return math.inf
+
     return max(s1,s2,s3,s4,s5,s6) - min(s1,s2,s3,s4,s5,s6)
 
 def find_points_in_lines(cur_points):
